6be33a7dde352d33b99f198cdacfa6ee610ad7a3
C0DERAI <riwatrj@gmail.com>
2019-08-15 22:09:15 +0100

    #Profile added

--- src/components/list/Profile.vue
+++ src/components/list/Profile.vue
@@ -3,47 +3,43 @@
     <q-page-container>
       <s-header></s-header>
 
-      <q-tabs  v-model="tab" animated  active-color="secondary"
+      <q-tabs v-model="tab" animated  active-color="secondary"
             indicator-color="secondary" align="justify">
         <!-- <q-tab default name="me" slot="section" icon="mail" label="me" /> -->
         <q-tab name="enterprise" label="enterprise" />
         <q-tab name="member" label="members" />
       </q-tabs>
       <q-separator />
-      <q-tab-panels v-model="tab" animated>
+      <q-tab-panels v-model="tab" animated class="bg-grey-1">
          <q-tab-panel name="enterprise">
           <div style="width: 500px; max-width: 90vw;" class="q-mx-auto">
-            <q-card inline class="bigger q-ma-sm">
-              <img src="statics/parallax2.jpg" />
-
-              <q-card-section class="relative-position">
-                <q-btn
-                  fab
-                  color="secondary"
-                  icon="directions"
-                  class="absolute"
-                  style="top: 0; right: 8px; transform: translateY(-50%);"
-                />
-                <div class="ellipsis">Cafe Basilico</div>
-                <!-- <q-rating slot="subsection" :max="5"/> -->
-                <!-- <q-rating slot="subsection" v-model="stars" :max="5"/> -->
-                <div slot="right" class="row items-center q-mt-md">
-                  <q-icon name="place" />250 ft
+            <!-- Contact Card ~ #01 -->
+                <div id="enterprise-profile_card">
+                    <div class="card-banner">
+                        <div class="avatar" ><img src="statics/parallax2.jpg" /></div>
+                    </div>
+                    <div class="card-quick_info">
+                        <h3>Saugat enterprise</h3>
+                        <ul class="info-content">
+                          <li>Quotation <span>200</span></li>
+                          <li>Invoice <span>120</span></li>
+                          <li>Customer <span>30</span></li>
+                        </ul>
+                    </div>
+                    <q-separator></q-separator>
+                    <div class="card-body">
+                      <a href="/"><q-icon name="email"></q-icon> cabrown96@live.com</a>
+                      <a href="/"><q-icon name="phone"></q-icon> +1 123-456-7890</a>
+                      <a href="/"><q-icon name="place"></q-icon> Kathmandu, Nepal</a>
+                    </div>
+                    <div class="card-footer">
+                      <div class="social_media-icons">
+                          <a href="/" target="_blank">Facebook</a>
+                          <a href="/" target="_blank">Linkden</a>
+                          <a href="/" target="_blank">Twitter</i></a>
+                      </div>
+                    </div>
                 </div>
-              </q-card-section>
-              <q-card-section>
-                <p>Retailer</p>
-                <p class="text-faded">Pan no: 0987654321</p>
-              </q-card-section>
-              <q-separator />
-              <q-card-actions>
-                <q-btn flat icon>Opened now</q-btn>
-                <q-btn flat>
-                  <q-icon name="phone" />9860181513
-                </q-btn>
-                <!-- <q-btn flat color="secondary">Message</q-btn> -->
-              </q-card-actions>
-            </q-card>
           </div>
         </q-tab-panel>
   <!-- Member profile panel -->
@@ -90,20 +86,73 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.footer-btn {
-  border-radius: 0;
-  color: #41aaa8;
-  background: #fafafa;
-}
+.footer-btn 
+  border-radius 0
+  color #41aaa8
+  background #fafafa
+#me-card, #enterprise-profile_card 
+  background $primary-white
+  margin 0 auto
 
-.me-card, .enterprise-card {
-  background: $primary-white;
-  margin: 0 auto;
-}
-
-.tab-me, .tab-enterprise {
-  background: $tab-color;
-}
+.tab-me, .tab-enterprise 
+  background $tab-color
+  
+#enterprise-profile_card
+  color #424242
+  overflow hidden
+	border-radius 15px
+	text-align center
+  display block
+.avatar
+  width 100%
+  text-align center
+ img
+  width 400px
+  height 400px
+.card-banner
+	height 100%
+	width 100%
+	padding-top 30px
+	background-size cover
+	background-position center
+.card-body
+  width 50%
+  margin 0 auto
+  padding 20px
+  a
+    display block
+    padding 5px 0px
+    font-size 14px
+    color #9E9E9E
+    text-decoration none
+    webkit-transition .1s ease-in-out
+    transition .1s ease-in-out
+    &:hover
+      color inherit
+.card-footer
+  text-align center
+ .social_media-icons
+    margin-top 10px
+    padding 15px 0px
+    background-color #FAFAFA
+    ,a 
+      padding 10px
+      display inline
+      transition .1s ease-in-out      
+      &:hover
+        font-size 20px
+.card-quick_info
+  h3
+    font-size 2rem
+    text-align center
+    padding 0
+    margin 0
+  ul
+    list-style none
+    li
+     font-size 1.2rem
+     display inline
+     padding 0 20px
 </style>
 
 
