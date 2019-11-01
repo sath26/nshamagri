fb0e937fbd73da67e1b52e0de8dcfaa4c5f0bf04
C0DERAI <riwatrj@gmail.com>
2019-11-01 00:29:54 +0000

    #result card added #minor bug with scrolling the list to be fixed #responsive 90%

--- src/layouts/Search.vue
+++ src/layouts/Search.vue
@@ -14,11 +14,11 @@
             active-color="secondary"
             indicator-color="secondary"
             align="left">
-            <q-tab name="enterprise" label="Enterprise" ></q-tab>
-            <q-tab name="quotation" label="Quotation" ></q-tab>
+            <q-tab name="enterprise" label="Enterprise"></q-tab>
+            <q-tab name="quotation" label="Quotation"></q-tab>
             <q-tab name="bought" label="Bought" ></q-tab>
             <q-tab name="sold" label="Sold" ></q-tab>
-            <q-btn-dropdown auto-close stretch flat label="Now">
+            <q-btn-dropdown auto-close stretch flat label="Now" class="colorGreen">
                 <q-list>
                   <q-item clickable @click="tab = 'now'">
                     <q-item-section>Now</q-item-section>
@@ -29,18 +29,21 @@
                   </q-item>
                 </q-list>
             </q-btn-dropdown>
-            <!-- Filter ui -->
-            <q-btn flat color="teal" @click="filter = true" > 
+            <!-- Filter dropdown button -->
+            <q-btn flat class="colorGreen" @click="filter = true" > 
               <q-avatar square size="25px" class="q-mr-sm">
                 <q-img class="filter-icon" src="../statics/icons/Filter-icon.svg" alt="filter icon"></q-img>
               </q-avatar>
               Filter
             </q-btn>
-            <q-dialog v-model="filter" transition-show="flip-down" transition-hide="flip-up">
-              <q-card style="width: 400px; max-width: 80vw;">
+            <!-- close button -->
+            <q-btn flat @click="searchPopout = false" class="close-btn" label="X" color="negative"></q-btn>
+            <!-- filter drop down menu content -->
+            <q-dialog  v-model="filter" transition-show="flip-down" transition-hide="flip-up">
+              <q-card style="width: 400px; max-width: 80vw;" class="q-pa-md">
                 <!-- category section -->
                 <q-card-section>
-                  <q-btn-dropdown auto-close stretch flat label="Category" class="dd dd-category q-pa-sm">
+                  <q-btn-dropdown auto-close stretch flat :label="categorySelection" class="dd dd-category q-pa-sm">
                 <q-list >
                   <q-item v-for="category in categories" tag="label" v-ripple class="q-pr-xl">
                     <q-item-section avatar>
@@ -97,7 +100,7 @@
                 <!-- done section -->
                 <q-card-section>
                   <q-item>
-                        <q-btn flat color="teal">Done</q-btn>
+                        <q-btn flat label="done" color="teal" v-close-popup></q-btn>
                       </q-item>
                 </q-card-section>
               </q-card>
@@ -107,14 +110,41 @@
           <q-separator ></q-separator>
           <q-tab-panels v-model="tab" animated>
             <q-tab-panel name="enterprise">
-              <div class="list-card_container">
-
+              <!-- Card -->
+              <q-card v-for="n in 2" flat class="enterprise-card_container">
+                <q-card-section class="test">
+                  <div class="list-card_container row items-center shadow-2 q-my-sm">
+                <div class="usr-img_container col">
+                  <img class="usr-img" src="../statics/guy-avatar.png" alt="dummy image">
+                </div>
+                <div class="info-name_container col-2"><p class="info-name_txt">Nep Enterprise <br><span class="usr-name_txt">Saugat Thapa</span></p></div>
+                <div class="info-distance_container info-container col">
+                  <img class="info-icons distance-icon" src="../statics/icons/distance-icon.svg" alt="distance icon"> 
+                  <div class="distance-txt_info txt-info"><p class="info-header">Distance<br><span class="info-value">5km</span></p></div>
+                </div>
+                <div class="info-invoice_container info-container col">
+                  <img class="info-icons invoice-icon" src="../statics/img/Invoice-graphics.png" alt="invoice icon"> 
+                  <div class="invoice-txt_info txt-info"><p class="info-header">Invoice<br><span class="info-value">2412341</span></p></div>
+                </div>
+                <div class="info-contact_container info-container col">
+                  <img class="info-icons contact-icon" src="../statics/icons/contact-icon.svg" alt="contact icon"> 
+                  <div class="contact-txt_info txt-info"><p class="info-header">Phone no.<br><span class="info-value">+977 987231764</span></p></div>
+                </div>
+                 <div class="info-email_container info-container col">
+                  <img class="info-icons email-icon" src="../statics/icons/email-icon.svg" alt="email icon"> 
+                  <div class="email-txt_info txt-info"><p class="info-header">Email address<br><span class="info-value">Saugat@example.com</span></p></div>
+                </div>
+                 <div class="info-customer_container col">
+                      <img class="customer-icon" src="../statics/icons/customers-icons.svg" alt="customer icon"> 
+                      <div class="customer-txt_info "><p class="info-header">Customers<br><span class="info-value">234</span></p></div>
+                </div>
+                <p class="info-date_txt">Date modified - 12/02/2019</p>
               </div>
+                </q-card-section>
+              </q-card>
             </q-tab-panel>
-
             <q-tab-panel name="quotation">
-              <div class="text-h6">Alarms</div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <p class="text-black">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nam mollitia architecto ab doloremque omnis! Atque beatae consequuntur quo architecto.</p>
             </q-tab-panel>
   
             <q-tab-panel name="bought">
@@ -136,7 +166,7 @@ export default {
       searchPopout: false,
       searchBox: '',
       text: '',
-      categorySelection:"all",
+      categorySelection:"category",
       categories: [
         {name:'All categories', value:'all'},
         {name:'Electornics', value:'electronic'},
@@ -160,6 +190,8 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  .colorGreen
+    color #4B7D6E
   .search-extension
     width 100%
     height 100%
@@ -169,15 +201,77 @@ export default {
     border none
     .my-card
       border-radius 0px
-  
-.filter-dropdown_content-list_item
-  background transparent
-  padding 0
-  margin 0
-.dd
-  width 100%
-.dd-price, .dd-location
-  padding 9px
-.price-input_field
-  width 50%
+  .filter-dropdown_content-list_item
+    background transparent
+    padding 0
+    margin 0
+  .dd
+    width 100%
+  .dd-price, .dd-location
+    padding 9px
+  .price-input_field
+    width 50%
+.close-btn
+  position absolute
+  right 10px
+// card styling
+.list-card_container
+  overflow hidden
+  color #424242
+  vertical-align center
+  position relative
+  .info-container
+    text-align center
+    padding 10px
+  .usr-img_container
+    width 10vw
+    height 10vw
+    // padding-top 10px
+    // padding-bottom 5px 
+    // margin 0
+    // background red
+    overflow hidden
+  .usr-img
+    width 10vw
+    height 10vw
+    // margin-left 10px
+  .info-name_container
+    text-align left
+    color #256856
+  .info-name_txt
+    font-size 2vw
+    line-height 25px
+    .usr-name_txt
+      font-size 1.1vw
+  .info-icons
+    width 40px
+    height 40px
+    margin-right 10px  
+    transform translateY(5px)
+    display inline-block
+  .info-header
+    font-weight 700
+    font-size 1.1vw
+    line-height 25px
+  .info-value
+    font-weight 400
+  .txt-info
+    display inline-block
+  .distance-text_info
+    display inline-block
+  .info-customer_container
+    text-align center
+  .customer-icon
+    width 45px
+    height 35px
+    transform translateY(5px) 
+  .email-icon, .contact-icon
+    width 30px
+    height 30px
+  .info-date_txt
+    font-size 1vw
+    opacity .8
+    position absolute
+    bottom -5%
+    right 2%
 </style>
