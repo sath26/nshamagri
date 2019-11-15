4c232599f207758e46538beed8095704ce728986
C0DERAI <riwatrj@gmail.com>
2019-11-15 22:12:29 +0000

    # Notification contents added # Scroll bar removed # content alignment fixed

--- src/layouts/Notification.vue
+++ src/layouts/Notification.vue
@@ -6,13 +6,16 @@
         <strong>Notification</strong>
       </q-tooltip>
     </q-btn>
-    <div :class="{notiBoxActive:notificationBox}" class="notification-box">
-      <q-card class="container-card" label="Notification">
-        <q-card-section class="text-black">
-          <div class="usr-info"><img src="../statics/boy-avatar.png" alt="usr-img"> <p>Riwat Rai</p></div>
-          <div class="notifi-title"></div>
-          <div class="notifi-info_txt"></div>
-          <div class="notifi-info_footer"></div>
+    <div :class="{notiBoxActive:notificationBox}" class="notification-box shadow-8" >
+      <q-card v-for="n in 10" class="container-card " label="Notification">
+        <q-card-section class="text-black row">
+          <div class="usr-info_img col-1"><img src="../statics/boy-avatar.png" alt="usr-img"> <p>Riwat Rai</p></div>
+          <div class="txt-content col q-ml-xl">
+            <div class="notifi-title"><h6>New message from Riwat Rai</h6></div>
+            <div class="notifi-info_txt"><p>Hello, I want to discuss about the recent order that I've made
+on December 1st....</p></div>
+            <div class="notifi-info_footer"><p>example@gmail.com</p><p>05:40 pm </p></div>
+          </div>
         </q-card-section>
       </q-card>
     </div>
@@ -34,18 +37,38 @@ export default {
 <style lang="stylus" scoped>
 @import "..\css\layouts-style\header.styl";
   .notification-box
-    width 80vw
-    height 100%
+    width 60vw
+    height 90vh
     position absolute
-    right -90px
-    top 54px
+    right 0
+    top 0
     display none
+    overflow scroll
+    transform translateY(-1.6%) 
+    &::-webkit-scrollbar 
+      width 0px
     .container-card
       border-radius 0
-      .usr-info
+      .usr-info_img
+        width 60px
+        height 85px
+        p
+          margin 0
         img
           width 60px
           height 60px
+      .txt-content
+        h6
+          margin 0
+        p
+          margin 0
+        .notifi-info_footer
+          font-size .7em
+          float right
+          p
+            display inline-block
+            &:nth-child(1)
+              margin-right 30px
   .notiBoxActive
     display block
     animation notiInAnimation 500ms ease forwards
